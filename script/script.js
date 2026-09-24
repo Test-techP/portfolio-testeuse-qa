5037379968c2e9db9747bb930009f5b712759e3a
Test-techP <pauline.puga.pp@gmail.com>
2026-09-24 15:02:06 +0200

    a11y: corriger la hierarchie des titres

--- script/script.js
+++ script/script.js
@@ -41,7 +41,7 @@ function createSkillsFromJSON() {
                     <div class="card skillsText">
                         <div class="card-body">
                             <img src="./images/${item.image}" alt="" />
-                            <h4 class="card-title mt-3">${item.title}</h4>
+                            <h3 class="card-title mt-3">${item.title}</h3>
                             <p class="card-text mt-3">${item.text}</p>
                         </div>
                     </div>
@@ -81,7 +81,7 @@ function createPortfolioFromJSON() {
                         style="width:100%"
                     >
                     <div class="card-body">
-                        <h4 class="card-title">${item.title}</h4>
+                        <h3 class="card-title">${item.title}</h3>
                         <p class="card-text">${item.text}</p>
                         <div class="text-center">
                             <a href="${item.link}" class="btn btn-success">Lien</a>
